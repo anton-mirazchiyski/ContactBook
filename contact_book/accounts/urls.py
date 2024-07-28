04c6666ca7b779a1f8aeed2181ac21e552ef485d
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-07-28 23:10:12 +0300

    Functionality and style for creating an account

--- contact_book/accounts/urls.py
+++ contact_book/accounts/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path, include
+
+from contact_book.accounts.views import AccountCreateView
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('create/', AccountCreateView.as_view(), name='account_create')
+]
--- contact_book/accounts/views.py
+++ contact_book/accounts/views.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import BaseUserCreationForm
 from django.shortcuts import render
+from django.views import generic as views
 
-# Create your views here.
+
+UserModel = get_user_model()
+
+
+class FormControlMixin:
+    fields = ()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control border border-dark border-2'
+
+
+class UserCreationForm(FormControlMixin, BaseUserCreationForm):
+    fields = ('username', 'password1', 'password2')
+
+
+class AccountCreateView(views.CreateView):
+    model = UserModel
+    form_class = UserCreationForm
+    template_name = 'accounts/account-create.html'
