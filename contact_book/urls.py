e4464a22d21750aa65f3e78e3279668d60f833e7
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-07-30 21:32:09 +0300

    Create app "contacts"

--- contact_book/urls.py
+++ contact_book/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('contact_book.accounts.urls')),
+    path('contacts/', include('contact_book.contacts.urls')),
 ]
