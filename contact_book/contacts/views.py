89035c71a44e62d4c466910c259dbbaf8691626e
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-08-08 20:28:58 +0300

    View to display all contacts

--- contact_book/contacts/views.py
+++ contact_book/contacts/views.py
@@ -2,13 +2,19 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.views import generic as views
 
+from contact_book.contacts.models import Category
+
 UserModel = get_user_model()
 
 
 def show_all_contacts(request):
     current_account = UserModel.objects.get(username=request.user.username)
-    contacts = current_account.contact_set.all()
+    categories = Category.objects.all()
 
-    context = {'contacts': contacts}
+    context = {}
+    for category in categories:
+        name = str(category).lower() + '_contacts'
+        contacts = current_account.contact_set.filter(category=category)
+        context[name] = contacts
 
     return render(request, 'contacts/contacts-all.html', context)
