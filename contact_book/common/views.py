78b4c31335ef3dae0853c240a17e6ed18450b319
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-08-16 21:06:55 +0300

    Start index page

--- contact_book/common/views.py
+++ contact_book/common/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from contact_book.core.accounts_utils import get_current_account
+
+UserModel = get_user_model()
+
+
+def index(request):
+    all_contacts = []
+    try:
+        current_account = get_current_account(request)
+        all_contacts = current_account.contact_set.all()
+    except UserModel.DoesNotExist:
+        current_account = None
+
+    context = {
+        'total_number_of_contacts': len(all_contacts),
+    }
+
+    return render(request, 'common/index.html', context)
