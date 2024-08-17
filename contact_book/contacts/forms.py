a3948c7bca0d0140d3fc723dfc3a4bc73695d2d0
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-08-17 22:58:03 +0300

    Update and refactor contact forms

--- contact_book/contacts/forms.py
+++ contact_book/contacts/forms.py
@@ -4,23 +4,10 @@ from contact_book.contacts.models import Contact
 from contact_book.core.mixins import FormControlMixin
 
 
-class ContactCreateForm(FormControlMixin, forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        fields = {key: value for key, value in self.fields.items() if key != 'category'}
-        self.apply_bootstrap_classes(fields)
+class ContactBaseForm(FormControlMixin, forms.ModelForm):
+    ADDRESS_FIELD_ROWS = 10
+    ADDRESS_FIELD_COLUMNS = 28
 
-    class Meta:
-        model = Contact
-        fields = ['name', 'phone_number', 'email', 'category']
-
-        widgets = {
-            'name': forms.TextInput(attrs={'autofocus': True}),
-            'category': forms.Select(attrs={'class': 'form-select border border-dark border-2'})
-        }
-
-
-class ContactEditForm(FormControlMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         fields = {key: value for key, value in self.fields.items() if key != 'category'}
@@ -30,7 +17,23 @@ class ContactEditForm(FormControlMixin, forms.ModelForm):
         model = Contact
         exclude = ['account']
 
+
+class ContactCreateForm(ContactBaseForm):
+    class Meta(ContactBaseForm.Meta):
+        fields = ['name', 'phone_number', 'email', 'category']
+
         widgets = {
-            'address': forms.Textarea(attrs={'rows': 10, 'cols': 25}),
+            'name': forms.TextInput(attrs={'autofocus': True}),
+            'category': forms.Select(attrs={'class': 'form-select border border-dark border-2'})
+        }
+
+
+class ContactEditForm(ContactBaseForm):
+    class Meta(ContactBaseForm.Meta):
+        widgets = {
+            'address': forms.Textarea(attrs={
+                'rows': ContactBaseForm.ADDRESS_FIELD_ROWS,
+                'cols': ContactBaseForm.ADDRESS_FIELD_COLUMNS
+            }),
             'category': forms.Select(attrs={'class': 'form-select border border-dark border-2'})
         }
